8e88e785feb5190d98627048c28726378eb63118
SupCodeTech <111235455+SupCodeTech@users.noreply.github.com>
2023-06-28 15:04:41 +0800

    Update Cavern_Report_Lung_Cavity_X_Aixs.py

--- Cavern_Report_Lung_Cavity_X_Aixs.py
+++ Cavern_Report_Lung_Cavity_X_Aixs.py
@@ -1,11 +1,5 @@
-# 代码区块 （六）组输出
-# 代码区块编码：ABAB0
-
-# 读取已经提取的JPG文件集合, 并提取图片的目标检测框内的像素(构建边界框自适应算法)，背景设置为0
-
 import cv2 as cv
 import matplotlib.pyplot as plt
-from google.colab.patches import cv2_imshow
 import math
 import pathlib
 from skimage.io import imread
@@ -18,33 +12,35 @@ import SimpleITK as sitk
 from argparse import ArgumentParser
 from PIL import Image, ImageDraw
 import numpy as np
-
+import nibabel as nib
+from nibabel.testing import data_path
+import os
 
 def calculate_histogram(image):
-    # 计算直方图
+    
     histogram = np.histogram(image.flatten(), bins=256, range=[0, 256])
     return histogram
 
 def sort_histogram_frequencies(histogram):
-    # 对直方图频率进行排序
+    
     frequencies = histogram[0]
-    pixel_values = histogram[1][:-1]  # 像素值数组
-    valid_indices = np.where(pixel_values >= 5)  # 有效的像素值索引
-    sorted_indices = np.argsort(frequencies[valid_indices])[::-1]  # 按频率降序排序的索引
-    sorted_frequencies = frequencies[valid_indices][sorted_indices]  # 排序后的频率
-    sorted_pixel_values = pixel_values[valid_indices][sorted_indices]  # 排序后的像素值
+    pixel_values = histogram[1][:-1]  
+    valid_indices = np.where(pixel_values >= 5)  
+    sorted_indices = np.argsort(frequencies[valid_indices])[::-1]  
+    sorted_frequencies = frequencies[valid_indices][sorted_indices]  
+    sorted_pixel_values = pixel_values[valid_indices][sorted_indices]  
     return sorted_pixel_values, sorted_frequencies
 
 def find_pixel_differences(sorted_pixel_values):
-    # 寻找像素值之差大于40的两个像素
+    
     for i in range(1, len(sorted_pixel_values)):
         difference = abs(sorted_pixel_values[0] - sorted_pixel_values[i])
-        if difference > 40:
+        if difference > 30:
             return sorted_pixel_values[0], sorted_pixel_values[i]
     return None
 
 def plot_histogram(histogram, pixel_1, pixel_2):
-    # 绘制直方图
+    
     plt.figure()
     plt.title('Histogram')
     plt.xlabel('Pixel Value')
@@ -80,10 +76,6 @@ def main():
   threshold_value = 0
   extract_threshold_value = 0
 
-  import nibabel as nib
-  from nibabel.testing import data_path
-  import os
-
   for files_num in range(60):
 
     if files_num < 10:
@@ -98,15 +90,13 @@ def main():
     image_type = []
 
     count_ = 0
-    a = [] # 属于这个文件的所有id
+    a = [] 
     for p in range(len(data_pd_)):
       if id[p] == nill_file:
         count_ = count_ + 1
         a.append(p)
 
     if len(a) > 0:
-
-      # 计算目录图片数量
 
       imgs = nib.load(args.Cavern_Detection_Train_CT + '/' + nill_file + '.nii.gz')
 
@@ -141,25 +131,16 @@ def main():
         for i in range(image_count):
 
           img_read_list_piexls_count = []
-
-          # 读取已经被提取Mask部分的图像
           
           srcs = cv2.imread(args.Cavern_Detection_Train_CT_PNG + '/' + nill_file + '/{}.png'.format(i))
           srcs = cv2.cvtColor(srcs, cv2.COLOR_BGR2GRAY)
 
-          
           img_read_lists.append(srcs)
-
         
         abc_stacks = np.stack(img_read_lists, axis = 0)
 
-        # cv2_imshow(abc_stacks[:,50,:])
-
         print("abc_stacks.shape: " + str(abc_stacks.shape))
-        # abc_stack_ = np.stack(img_read_list_, axis = 0)
-
-      # 正面切片 - Front
-
+          
       while slices < len(a):
         for i in range(512):
 
@@ -192,29 +173,16 @@ def main():
             patch_ee = abc_stacks[css:dss, i, ass : bss]
             patch_ee_ = abc_stacks[z1:z2, i ,x1 : x2]
 
-            # print("修复前： *****************")
+            histogram = calculate_histogram(patch_ee)
 
-            # cv2_imshow(cv2.resize(patch_ee[bbz_len:2*bbz_len, bby_len:2*bby_len], (bby_len, bby_len)))
+            sorted_pixel_values, sorted_frequencies = sort_histogram_frequencies(histogram)
+              
+            pixel_1, pixel_2 = find_pixel_differences(sorted_pixel_values)
 
+            threshold_value = (pixel_1 + pixel_2) // 2
 
-            # patch_ees_ = abc_stacks[i, y1 : y2, x1:x2]
-            # patch_ees_ = np.dstack((patch_ee, patch_ees_, patch_ees_))
-
-            ###################### CONSOLIDATION 阈值提取 ##########################
-
-            # 计算灰度图像的平均值
-
-            # ksc = cv2.cvtColor(patch_ee, cv2.COLOR_BGR2GRAY)
-
-            mean_val = np.mean(patch_ee)
-
-            # gray = patch_ee
-
-            # # cv2_imshow(gray)
-
-            # # 使用平均灰度值作为阈值进行二值化
-            threshold, binary = cv2.threshold(patch_ee, mean_val, 255, cv2.THRESH_BINARY)
-
+            threshold, binary = cv2.threshold(patch_ee, threshold_value, 255, cv2.THRESH_BINARY)
+              
             for pz in range(binary.shape[0]):
               for px in range(binary.shape[1]):
                 # abc_stacks[i][py + (y1)][px + (x1)] = binary[py][px]
@@ -225,39 +193,17 @@ def main():
                 elif binary[pz][px] > 0:
                   k_lesion[pz + css, i, px + ass] = 255
 
-
             binary = binary[bbz_len:2*bbz_len, bbx_len:2*bbx_len]
-            
 
-            # 轮廓发现
             contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-
-            ###################### Lung cavity 阈值提取 ##########################
-
-
-            # binary = cv2.resize(binary, (bby_len, bby_len))
 
             binary = np.dstack((binary, binary, binary))
 
-            # print("contours.shape: " + str(contours.shape))
-
-            # 创建与原始图像大小相同的全黑图像
             mask = np.zeros_like(binary)
 
-            # 循环所有轮廓，并绘制它们
             for sr in range(len(contours)):
-                # 如果轮廓具有父轮廓，则表示它是空洞轮廓
                 if hierarchy[0][sr][3] != -1:
-                    # 绘制轮廓
                     cv2.drawContours(mask, contours, sr, (255, 255, 255), cv2.FILLED)
-
-
-            # cv2_imshow(mask)
-
-            # mask = cv2.resize(mask, (bbz_len, bby_len))
-
-            # print("修复后： *****************")
 
             left_up_corner = False
             right_up_corner = False
@@ -297,8 +243,6 @@ def main():
                 if newimg[rt1][rt2][rt3] > 0:
                   if k_lungcavity[rt1][rt2][rt3] > 0 and newimg[rt1][rt2][rt3] != 1:
                     newimg[rt1][rt2][rt3] = 3
-
-          # 记得 k 换为 newimg
 
           newimg = newimg.transpose(2,1,0)
           final_img = nib.Nifti1Image(newimg, imgs.affine)
